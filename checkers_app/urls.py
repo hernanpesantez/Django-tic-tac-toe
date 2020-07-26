bb9f2ce443003b5991dc866dd1427cb74ce29acb
doris moreno <dorismoreno927@gmail.com>
2020-07-25 22:43:35 -0400

    puzzleDoris

--- checkers_app/urls.py
+++ checkers_app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_view, about_view, contact_view, puzzle_view, tictactoe_view, index
+from .views import home_view, about_view, contact_view, puzzle_view, tictactoe_view, index, puzzledoris_view
 
 urlpatterns = [
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('about/', about_view, name='about'),
     path('contact/', contact_view, name='contact'),
     path('puzzle/', puzzle_view, name='puzzle'),
+    path('puzzledoris/', puzzledoris_view, name='puzzledoris'),
     path('tictactoe/', tictactoe_view, name='tictactoe'),
     path('handler/',index, name='index')
 
